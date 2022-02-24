cc63eb553f2a97bfbbb7ff21babb0c8ab9bc5112
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-24 11:35:00 +0800

    Used Object.keys() method to get the length

---
+++ exer.js
@@ -0,0 +1,9 @@
+function run(){
+
+    // Declare an object
+   const student = { name : "David Rayy", sclass : "VI", rollno : 12 };
+  
+            // Using Object.keys() method to get length
+            document.getElementById("output").innerHTML = Object.keys(student).length;
+
+}
